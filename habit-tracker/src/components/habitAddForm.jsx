585cf04c8c35077b7664789ae04fd509349b4439
Sc-202010122235\Administrator <azskw1101@gmail.com>
2020-11-04 01:22:35 +0900

    add navbar, form

---
+++ habit-tracker/src/components/habitAddForm.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class HabitAddForm extends Component {
+  inputRef = React.createRef();
+  onSubmit = (event) => {
+    event.prevenDefault();
+    console.log(this.inputRef);
+  };
+  render() {
+    return (
+      <form className="add-form" onSubmit={this.onSubmit}>
+        <input
+          ref={this.inputRef}
+          type="text"
+          className="add-input"
+          placeholder="Habbit"
+        />
+        <button className="add-button">Add</button>
+      </form>
+    );
+  }
+}
+
+export default HabitAddForm;
