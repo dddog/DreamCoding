80febfe64365a9cdb6fc6dbb2389574efc6047d1
dddog <azskw1101@gmail.com>
2020-12-14 17:01:22 +0900

    search bar add

--- youtube/src/app.jsx
+++ youtube/src/app.jsx
@@ -1,12 +1,30 @@
 import { useEffect, useState } from "react";
-import "./app.css";
+import styles from "./app.module.css";
+import SearchHeader from "./components/search_header/search_header";
 import VideoList from "./components/video_list/video_list";
 
 function App() {
   const [videos, setVideos] = useState([]);
+  const search = (query) => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&chart=mostPopular&maxResults=25&q=${query}&type=video&key=AIzaSyAzu5to90FYCN4QPV12GGynwMiVGCLURMI`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        result.items.map((item) => ({ ...item, id: item.id.videoId }))
+      )
+      .then((items) => setVideos(items))
+      .catch((error) => console.log("error", error));
+  };
 
   useEffect(() => {
-    const requestOptions = {
+    var requestOptions = {
       method: "GET",
       redirect: "follow",
     };
@@ -19,7 +37,12 @@ function App() {
       .then((result) => setVideos(result.items))
       .catch((error) => console.log("error", error));
   }, []);
-  return <VideoList videos={videos} />;
+  return (
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <VideoList videos={videos} />
+    </div>
+  );
 }
 
 export default App;
