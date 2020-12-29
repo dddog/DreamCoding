1d33c55de6f4ec3bffce0bf58fdde5557289e8aa
Sc-202010122235\Administrator <azskw1101@gmail.com>
2020-12-29 22:16:09 +0900

    add CardAddForm component

--- card-maker/src/components/editor/editor.jsx
+++ card-maker/src/components/editor/editor.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import CardEditForm from "../card_editor_form/card_edit_form";
+import CardAddForm from "../card_add_form/card_add_form";
 import styles from "./editor.module.css";
 
-const Editor = ({ cards }) => (
+const Editor = ({ cards, addCard }) => (
   <section className={styles.editor}>
     <h1 className={styles.title}>Card maker</h1>
     {cards.map((card) => (
       <CardEditForm card={card} key={card.id} />
     ))}
+    <CardAddForm onAdd={addCard} />
   </section>
 );
 
